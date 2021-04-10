cdef376947e19d0121982fa7fdcc00e05e4d1132
talzaken <talzaken@gmail.com>
2021-04-10 11:54:27 -0400

    test commit

--- flask_app.py
+++ flask_app.py
@@ -97,7 +97,7 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 @app.route('/update_server', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        repo = git.Repo('https://github.com/tal-z/PersonalWebsite')
+        repo = git.Repo('mysite/PersonalWebsite')
         origin = repo.remotes.origin
         origin.pull()
         return ('Updated PythonAnywhere successfully', 200)
