b9d35a98d151dc9b3f4537cc02d0f133b99d2c3f
talzaken <talzaken@gmail.com>
2021-04-10 18:28:51 -0400

    updates

--- flask_app.py
+++ flask_app.py
@@ -28,7 +28,7 @@ except:
 
 matplotlib.use('Agg')
 
-"""jkhasdasdhj"""
+""" :) """
 
 def get_revision_timestamps(TITLE):
     # base URL for API call
@@ -115,7 +115,7 @@ def webhook():
     if request.method == 'POST':
         github_secret_token = os.getenv('github_secret_token')
         x_hub_signature = request.headers.get('X-Hub-Signature')
-        if not is_valid_signature(x_hub_signature, request.data, github_secret_token):
+        if is_valid_signature(x_hub_signature, request.data, github_secret_token):
             repo = git.Repo('mysite/PersonalWebsite')
             origin = repo.remotes.origin
             origin.pull()
